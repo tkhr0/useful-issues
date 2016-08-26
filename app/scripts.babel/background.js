4119e3c2bece4ba080aeda5672d932ab0146f0f7
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2016-08-27 01:14:04 +0900

    init commit

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,10 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(details => {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({text: '\'Allo'});
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
+console.log(chrome.runtime.getManifest().name);
