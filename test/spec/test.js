c33710d91fc50be247bb2f8674c1cea9738bfdd6
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2017-05-03 20:16:09 +0900

    add first test

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,14 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
 (function () {
-  'use strict';
 
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-
+        expect('everthing').to.be.ok;
       });
     });
   });
